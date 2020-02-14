99a3e15fb795fe14063986798a7dd6cf3e40d31f
alwilmet <wilmet.alex@gmail.com>
2020-02-14 09:17:43 +0100

    first commit

---
+++ function/graph.py
@@ -0,0 +1,26 @@
+
+def write_followers_gml(followers ,users):
+    with open('./following2.gml', 'w') as f:
+        f.write("Creator \"Following\"\n")
+        f.write("graph\n[\n")
+        for user in users:
+            f.write("  node\n  [\n")
+            f.write("   id %s\n" %user)
+            f.write("  ]\n")
+        for row in followers.itertuples():
+            fols = row.following
+            for fol in fols:
+                f.write("  edge\n  [\n")
+                f.write("   source %s\n" % row.screen_name)
+                f.write("   target %s\n" % fol)
+                f.write("   weight 1\n")
+                f.write("  ]\n")
+        f.write("]\n")
+
+
+"""
+from igraph import *
+g = Graph.Read_GML("following.gml")
+p = g.community_multilevel()
+q = g.modularity(p)
+print(q)"""
